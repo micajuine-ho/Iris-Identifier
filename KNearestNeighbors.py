408f251c8096b6a536b140bad1d20cc4da5ef0b9
Micajuine Ho <ho.micajuine@gmail.com>
2018-01-16 23:23:48 -0500

    Added K selection

--- KNearestNeighbors.py
+++ KNearestNeighbors.py
@@ -10,7 +10,10 @@
 #
 # The iris dataset can be found here: https://en.wikipedia.org/wiki/Iris_flower_data_set
 
-from scipy.spatial import distance 
+from scipy.spatial import distance
+import heapq 
+import collections 
+
 def dist(a,b):
     return distance.euclidean(a,b)
 
@@ -19,21 +22,40 @@ class My_KNN():
         self.x_train = x_train
         self.y_train = y_train
 
-    def predict(self, x_test):
+    def predict(self, x_test, k):
         predictions = []
         for row in x_test:
-            predictions.append(self.closest(row))
+            predictions.append(self.closest(row, k))
         return predictions
 
-    def closest(self, row):
-        best_index = 0
-        best_dist = dist(row, self.x_train[0]) 
-        for i in range (1, len(self.x_train)):
-            curr_dist = dist (row, self.x_train[i])
-            if curr_dist < best_dist:
-                best_dist = curr_dist
-                best_index = i
-        return self.y_train[best_index]
+    def closest(self, row, k):
+        #No built in library for max heaps, so just use negative distance
+        max_heap = []
+        most_occuring_targets = []
+
+        #Add the first k elements to heap, then for the rest of the elms
+        #check if the distance is smaller (larger) than the max (min), 
+        #and add it into the max (min) heap.
+        for i in range (0, len(self.x_train)):
+
+            #Turn distance to a negative so that we can maintain min heap
+            curr_dist = -1 * (dist (row, self.x_train[i]))
+
+            #Add first K elements in 
+            if i < k:
+                heapq.heappush(max_heap, (curr_dist, i)) 
+            
+            #Check if min element (max distance) is smaller than the curr dist
+            elif curr_dist > max_heap[0][0]:
+                heapq.heappop(max_heap)
+                heapq.heappush(max_heap, (curr_dist, i))
+
+        #Add the most occuring target to an array to find the most occuring one.
+        for j in range (0, len(max_heap)):
+            most_occuring_targets.append(self.y_train[max_heap[j][1]])
+
+        #Return most occuring target
+        return collections.Counter(most_occuring_targets).most_common(1)[0][0]
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
@@ -57,7 +79,10 @@ classifier = My_KNN()
 classifier.fit(x_train, y_train)
 
 # Make predicition using classifier and test data
-predicition = classifier.predict(x_test)
+predicition = classifier.predict(x_test, 3)
+
+# print(predicition)
+# print(y_test)
 
 # Validation 
 print (accuracy_score(predicition, y_test))
